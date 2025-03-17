141a7a698b13caf9d2a2cbd98550b0981a37f655
Bryan <bryan.guerin@viacesi.fr>
2025-03-17 19:25:57 +0100

    fix gitIgnore Add missing file

---
+++ src/main/java/com/bryan/spaceinvader/model/game/Rect.java
@@ -0,0 +1,21 @@
+package com.bryan.spaceinvader.model.game;
+
+import javafx.scene.canvas.Canvas;
+
+// Const of the rect around invaders and define different properties like gap, spacing and size
+// No idea how to name it better
+public class Rect {
+
+    public double leftGap;
+    public double topGap;
+    public double xGap;
+    public double yGap;
+
+    public Rect(Canvas canvas, GameConfig gameConfig) {
+        xGap = 2 * AbsInvader.SIZE;
+        yGap = 1.5 * AbsInvader.SIZE;
+        leftGap = ((int) ((canvas.getWidth() - gameConfig.getNumberOfColumns() * xGap + AbsInvader.SIZE) / 2));
+        topGap = canvas.getHeight() * 0.05;
+    }
+
+}
